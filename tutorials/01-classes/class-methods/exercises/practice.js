247fde5e3b2c3098f3583dfc0834ddcae515b9b9
lakshmi priya <lakshmipriyaark@gmail.com>
2026-06-01 17:11:50 -0500

    class-methods pratice -done

--- tutorials/01-classes/class-methods/exercises/practice.js
+++ tutorials/01-classes/class-methods/exercises/practice.js
@@ -17,10 +17,15 @@ class GameHero {
         It should take no parameters. Inside, add 25 to 'this.health' and return 
         a template literal string stating: "[name] drinks a potion and heals! Health: [health]"
     */
-}
+				drinkPotion(){
+					this.health += 25;
+					return `${champion.name} drinks a potion and heals! Health: ${champion.health}`;
+				}
+	}
 
 const champion = new GameHero("Arthur");
 
+console.log(champion.drinkPotion());
 // TODO: Test your newly engineered action and print the result.
 
 /** EXERCISE 2: CLEANING UP THE MEMORY TRAP **/
@@ -29,19 +34,21 @@ class EnemyOrc {
 	constructor(orcName) {
 		this.name = orcName;
 		this.rageMeter = 0;
+	}
 		// POOR ARCHITECTURE: This function gets copied into memory over and over!
-		this.gainRage = () => {
+		gainRage(){
 			this.rageMeter += 20;
 			return `${this.name} growls! Rage increased to ${this.rageMeter}.`;
-		};
-	}
+		}
+	
+}
 
 	/*
         TODO: Look at the bad implementation above, then move it out of the constructor.
         Remember that the syntax changes when you are no longer storing it as a property
         with an anonymous definition.
     */
-}
+
 
 const basicOrc = new EnemyOrc("Grom");
 

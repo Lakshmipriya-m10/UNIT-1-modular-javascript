4139a0de81b287b4c7bc53564e5c2bcb07d470af
lakshmi priya <lakshmipriyaark@gmail.com>
2026-06-02 13:48:25 -0500

    import .js done

--- tutorials/02-modules/commonjs-modules/exercises/practice/settings.js
+++ tutorials/02-modules/commonjs-modules/exercises/practice/settings.js
@@ -1,6 +1,6 @@
 const gameTitle = "Retro Arcade Realms";
 const maxPartySize = 4;
-
+module.export = { gameTitle, maxPartySize };
 /*
     TODO: The developer declared the assets but forgot to export them!
     Write the CommonJS export statement below to export both variables 

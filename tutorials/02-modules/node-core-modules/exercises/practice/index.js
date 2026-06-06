70fce040ad570ac6667656767e4eff931060d2ac
lakshmi priya <lakshmipriyaark@gmail.com>
2026-06-06 09:10:38 -0500

    npm-modules-practice done

--- tutorials/02-modules/node-core-modules/exercises/practice/index.js
+++ tutorials/02-modules/node-core-modules/exercises/practice/index.js
@@ -8,7 +8,9 @@
 /** EXERCISE **/
 
 // TODO: Use CommonJS syntax to import os, path, and fs
-
+const os = require("os");
+const path = require("path");
+const fs =require("fs");
 /*
     TODO: Build a local file generator script.
     1. Look up the total system memory of the computer using 'os.totalmem()'.
@@ -17,3 +19,17 @@
     4. Use 'fs.writeFileSync' to save that string into that path destination.
     5. Use 'fs.readFileSync' in a try/catch block to read from the new file.
 */
+
+const memoryStats = os.totalmem();
+console.log (`Total System Memory: ${memoryStats} bytes.`);
+const filePath = path.join(process.cwd(), "system.txt");
+console.log(filePath);
+const writeNewLine = fs.writeFileSync("system.txt","Processor: Intel Core i5-1235U");
+try{
+    const readFileContent = fs.readFileSync(filePath, "utf8");
+    console.log(readFileContent);
+}
+catch (error)
+{
+console.log("No file");
+}
--- tutorials/02-modules/npm-modules/exercises/practice/app.js
+++ tutorials/02-modules/npm-modules/exercises/practice/app.js
@@ -14,10 +14,13 @@
 /*
     TODO: Use the correct syntax (CommonJS or ES) to import chalk and gradient-string
 */
-
+import chalk from 'chalk';
+import gradient from 'gradient-string';
 /*
     TODO: Use 'gradient-string' to style a victory banner string.
     1. Create a variable named 'goldGradient' by passing the colors "yellow" and "orange" into the gradient tool.
     2. Apply that gradient to the string: "★ VICTORY! YOU FOUND THE ANCIENT GOLD REWARD ★"
     3. Log the stylized message to the console.
 */
+const goldGradient = gradient(["yellow", "orange"]);
+console.log(goldGradient('★ VICTORY! YOU FOUND THE ANCIENT GOLD REWARD ★'));
